f83fcb3e148c328a39d0de333fb72392275bc0d6
penach0 <josemanuel.penacho@gmail.com>
2023-03-24 21:26:55 +0000

    Add logic to the create action on Attendances
    - Also added strong parameters

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,10 @@
 class AttendancesController < ApplicationController
   def create
+    @attendance = Attendance.new(attendance_params)
   end
+
+  private
+    def attendance_params
+      params.require(:attendance).permit(:attendee_id, :attended_event_id)
+    end
 end
