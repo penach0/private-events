6823998be082ed5acbc4f22211f9438a81e3cd00
penach0 <josemanuel.penacho@gmail.com>
2023-03-24 20:38:36 +0000

    Create associations between models and Attendance

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :attendances, foreign_key: "attended_event_id"
+  has_many :attendees, through: :attendances
 
   validates :creator_id, :name, :date, :location, :description, presence: true
   validates :date, comparison: { greater_than: Time.current }
