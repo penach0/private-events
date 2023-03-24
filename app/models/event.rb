fc9560db700e70736329abd48e98dfc315ff277d
penach0 <josemanuel.penacho@gmail.com>
2023-03-24 21:59:45 +0000

    Fix associations
    - My associations through attendances were not working because the
    belongs_to associations on Attendance were not specifying the
    class names

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :attendances, foreign_key: "attended_event_id"
+  has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
 
   validates :creator_id, :name, :date, :location, :description, presence: true
