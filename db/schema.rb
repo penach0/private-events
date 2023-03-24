bf6ce51cf26eb8a1db52e259500015609983d3a6
penach0 <josemanuel.penacho@gmail.com>
2023-03-24 15:40:46 +0000

    Add description column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_081646) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_154003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_081646) do
     t.datetime "updated_at", null: false
     t.bigint "creator_id", null: false
     t.string "location"
+    t.text "description"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
