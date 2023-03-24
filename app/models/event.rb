f0c8211fe52e5da7ad70061a99b3fab647d9de41
penach0 <josemanuel.penacho@gmail.com>
2023-03-24 16:15:32 +0000

    Create validations for Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+
+  validates :creator_id, :name, :date, :location, :description, presence: true
+  validates :date, comparison: { greater_than: Time.current }
 end
