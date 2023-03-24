f751c171d288f57bedc494c4d2c77e0f86f575ce
penach0 <josemanuel.penacho@gmail.com>
2023-03-24 08:53:53 +0000

    Create Events new and create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,18 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.build_event
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
