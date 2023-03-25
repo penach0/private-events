5409cdef2d46360667bb3907ef57b1cb00fe1738
penach0 <josemanuel.penacho@gmail.com>
2023-03-25 07:41:29 +0000

    Create destroy action for Attendances
    - Also add route

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,14 @@
 class AttendancesController < ApplicationController
   def create
     @attendance = Attendance.create(attendance_params)
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @attendance = Attendance.find_by(attendance_params)
+    @attendance.destroy
+
     redirect_back(fallback_location: root_path)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'users/:id', to: "users#show"
 
   resources :events
-  resources :attendances, only: [:create]
+  resources :attendances, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
