082e7ea98a20d8115cfd0fb2d40404c690081fc5
penach0 <josemanuel.penacho@gmail.com>
2023-03-25 20:03:29 +0000

    Add simple navbar
    - Add simple styles to the navbar
    - Add link to root page
    - Turn Event class methods into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,15 +6,10 @@ class Event < ApplicationRecord
   validates :creator_id, :name, :date, :location, :description, presence: true
   validates :date, comparison: { greater_than: Time.current }
 
+  scope :past, -> { where("date < ?", Time.current) }
+  scope :upcoming, -> { where("date >= ?", Time.current) }
+
   def formatted_date
     date.strftime("%d/%m/%y at %H:%M" )
-  end
-
-  def self.past
-    where("date < ?", Time.current)
-  end
-
-  def self.upcoming
-    where("date >= ?", Time.current)
   end
 end
